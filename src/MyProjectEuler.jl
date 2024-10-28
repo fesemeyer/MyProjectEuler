235ac868abacfdaffe1785827360dc5352ecd53a
fesemeyer <felix.sebastian.meyer@rwth-aachen.de>
2024-10-28 16:30:29 +0100

    add core functions

--- src/MyProjectEuler.jl
+++ src/MyProjectEuler.jl
@@ -1,5 +1,27 @@
 module MyProjectEuler
 
-greet() = print("Hello World!")
+using BenchmarkTools
+
+export solve
+export Problem1
+
+abstract type Problem end
+
+function solve(ID::Int, benchmark = true)
+    problem = try
+        eval(Meta.parse("Problem$ID()"))
+    catch
+        error("Solution for problem $ID not yet implemented.")
+    end
+
+    if benchmark
+        bench = @benchmark solve($problem)
+        display(bench)
+    end
+    solve(problem)
+end
+
+# include all existing problem solutions
+include.(filter(contains(r".jl$"), readdir("src/solutions/"; join=true)))
 
 end # module MyProjectEuler
